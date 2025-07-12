0f024b1fdad7afc881ab146fbf1fabe4f0fff27f
Christopher Francisco <cjfg00gl3@gmail.com>
2025-07-12 14:26:09 -0400

    update(main.py): display first 5 rows of the train and test datasets

--- GETTING-STARTED/TITANIC-MACHINE-LEARNING-FROM-DISASTER/main.py
+++ GETTING-STARTED/TITANIC-MACHINE-LEARNING-FROM-DISASTER/main.py
@@ -3,4 +3,7 @@ from pandas import read_csv # the csvs contain the data you need to use
 test_csv = read_csv("titanic/test.csv") # read the test.csv file
 train_csv = read_csv("titanic/train.csv") # read the train.csv file
 
+print(test_csv.head()) # read the first 5 rows of the test_csv
+print(train_csv.head()) # read the first 5 rows of the train_csv
+
 
