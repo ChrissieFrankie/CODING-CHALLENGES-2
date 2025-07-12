009e73823d2484df82a3a1ece6562ea62bab079b
Christopher Francisco <cjfg00gl3@gmail.com>
2025-07-12 14:25:25 -0400

    update(main.py): to load titanic dataset csv files

---
+++ GETTING-STARTED/TITANIC-MACHINE-LEARNING-FROM-DISASTER/main.py
@@ -0,0 +1,6 @@
+from pandas import read_csv # the csvs contain the data you need to use
+
+test_csv = read_csv("titanic/test.csv") # read the test.csv file
+train_csv = read_csv("titanic/train.csv") # read the train.csv file
+
+
